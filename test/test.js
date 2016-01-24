311a1bc1042ed901ad08aa71a84b1b159b08a5af
Pranesh Ravi <praneshr@indix.com>
2016-01-24 23:05:56 +0530

    Fixing root path error and adding tests

--- test/test.js
+++ test/test.js
@@ -1,2 +1,32 @@
-const mocha = require('mocha');
-const assert = require('assert');
+const expect = require('expect');
+const fs = require('fs');
+
+describe('Testing on various Environment', function() {
+  afterEach(function() {
+    delete require.cache[require.resolve('../lib/index')];
+  });
+
+  it('It should return a config with default as true', function () {
+    process.env.NODE_ENV = 'default';
+    const lib = require('../lib/index');
+    expect(lib.getEnvConfig).toEqual({ default: true, production: false, staging: false });
+  });
+
+  it('It should return a config with production as true', function () {
+    process.env.NODE_ENV = 'production';
+    const lib = require('../lib/index');
+    expect(lib.getEnvConfig).toEqual({ default: false, production: true, staging: false });
+  });
+
+  it('It should return a config with staging as true', function () {
+    process.env.NODE_ENV = 'staging';
+    const lib = require('../lib/index');
+    expect(lib.getEnvConfig).toEqual({ default: false, production: false, staging: true });
+  });
+
+  it('It should return a config with default as true because test.js is not found in webpack folder', function () {
+    process.env.NODE_ENV = 'test';
+    const lib = require('../lib/index');
+    expect(lib.getEnvConfig).toEqual({ default: true, production: false, staging: false });
+  });
+});
